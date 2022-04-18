1bad43012f1c9eaa6fe6314ac0b70425a56b09a8
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-19 01:47:50 +0200

    app should start with the home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:currency_converter_app/screens/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: null,
+      home: HomePage(),
     );
   }
 }
