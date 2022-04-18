7ef0dbc2f854292da36be4b168d8b241bf41c28c
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-19 00:04:12 +0200

    add an appBar

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          "Currency Converter",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+            fontSize: 25,
+          ),
+        ),
+      ),
+    );
+  }
+}
