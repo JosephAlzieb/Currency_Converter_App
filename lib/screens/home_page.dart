8fdb7e21dae8aec7ace74cdd2a6e1dfb85e440ec
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-19 01:47:09 +0200

    Drop buttons with a floatation-button to swap between the currencies

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:currency_converter_app/widgets/drop_down_currencies.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,15 +13,16 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF212936),
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: const Color(0xFF212936),
         elevation: 0,
         centerTitle: true,
         title: const Text(
           "Currency Converter",
           style: TextStyle(
             fontWeight: FontWeight.bold,
-            color: Colors.black,
+            color: Colors.white,
             fontSize: 25,
           ),
         ),
@@ -28,23 +30,38 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextFormField(
-              decoration: InputDecoration(
-                icon: Icon(Icons.money),
+              decoration: const InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                // icon: Icon(
+                //   Icons.money,
+                //   color: Colors.white,
+                // ),
                 labelText: "Amount",
                 hintText: "enter the amount you want to convert here",
-                enabledBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(width: 3),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(width: 3,color: Colors.blue),
-                  borderRadius: BorderRadius.circular(15),
-                ),
               ),
               keyboardType: TextInputType.number,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                DropDownCurrencies(),
+                FloatingActionButton(
+                  backgroundColor: Colors.white,
+                  onPressed: () {},
+                  child: Icon(Icons.swap_horiz_outlined, color: Colors.black,),
+                ),
+                DropDownCurrencies(),
+              ],
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 50,
+              color: Colors.white,
+              child: Text("Result", textAlign: TextAlign.center,style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
             )
           ],
         ),
