7cd3a8b2b0d42c03721c9e5973d8ee5e4ecd6127
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-19 16:43:02 +0200

    refactoring

--- lib/widgets/drop_down_currencies.dart
+++ lib/widgets/drop_down_currencies.dart
@@ -1,32 +1,35 @@
 import 'package:flutter/material.dart';
 
-class DropDownCurrencies extends StatefulWidget {
-  const DropDownCurrencies({Key? key}) : super(key: key);
+class DropDownCurrencies extends StatelessWidget {
+  List<String> currencies;
+  String? hintText;
+  String? value;
+  Function onChanged;
 
-  @override
-  State<DropDownCurrencies> createState() => _DropDownCurrenciesState();
-}
-
-class _DropDownCurrenciesState extends State<DropDownCurrencies> {
-  List<String> currenxies = ["USD", "EUR"];
-  String? _value;
+  DropDownCurrencies(
+      {required this.currencies, required this.value,required this.hintText, required this.onChanged,});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: 120,
       decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
         color: Colors.white,
       ),
-      child: DropdownButton(
-        value: _value,
-        items: currenxies.map<DropdownMenuItem<String>>((String? item) => DropdownMenuItem<String>(child: Text(item!),value: item,)).toList(),
-        onChanged: (String? value) {
-          setState(() {
-            _value = value;
-          });
-        },
-        hint: const Text("from"),
-        icon: const Icon(Icons.arrow_drop_down_circle_outlined),
+      child: Center(
+        child: DropdownButton(
+          style: const TextStyle(fontSize: 25, color: Colors.black),
+          value: value,
+          items: currencies.map<DropdownMenuItem<String>>((String? item) =>
+              DropdownMenuItem<String>(child: Text(item!), value: item,))
+              .toList(),
+          onChanged: (String? value) {
+            onChanged(value);
+          },
+          hint: Text(hintText! , style: const TextStyle(fontSize: 25),),
+          icon: const Icon(Icons.arrow_drop_down_circle_outlined),
+        ),
       ),
     );
   }
