ffe92d85900c549591ad1f357c554df0b943a534
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-19 17:41:40 +0200

    to get the rate of the currencies to convert

--- lib/services/api_client.dart
+++ lib/services/api_client.dart
@@ -12,14 +12,33 @@ class ApiClient {
   Future<List<String>> getCurrencies() async {
     http.Response response = await http.get(currenciesUrl);
 
-    if(response.statusCode==200){
+    if (response.statusCode == 200) {
       var body = jsonDecode(response.body);
       var list = body["results"];
       List<String> currenciesList = (list.keys).toList();
       return currenciesList;
-    }
-    else {
+    } else {
       throw Exception("failed to connect to api");
+    }
+  }
+
+  Future<double> getRate(String from, String to) async {
+    final Uri ratUrl = Uri.https(
+      "free.currconv.com",
+      "/api/v7/convert",
+      {"apiKey":"249038a0891d75dc786d",
+        "q":"${from}_${to}",
+        "compact":"ultra"
+      },
+    );
+
+    http.Response response = await http.get(ratUrl);
+    if(response.statusCode == 200){
+      var body = jsonDecode(response.body);
+      return body["${from}_${to}"];
+    }
+    else{
+      throw Exception("failed to connect to the api");
     }
   }
 }
